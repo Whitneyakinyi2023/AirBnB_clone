18d7a76f456bfca9899729899959da7e10e44a0d
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-03-07 00:45:31 -0800

    console.py

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Console for our AirBnB clone"""
+
+
+try:
+    import gnureadline
+    import sys
+    sys.modules['readline']
+except ImportError:
+    pass
+
+import cmd
+"""Command line interpretor"""
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, args):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, args):
+        """Handles end of file signal"""
+        print("^D")
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
