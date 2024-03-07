7ef263f8836c9b63b13b4d87ed43e6c0eac2fe7b
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-03-07 00:53:43 -0800

    Handled empty line

--- console.py
+++ console.py
@@ -25,6 +25,10 @@ class HBNBCommand(cmd.Cmd):
         print("^D")
         return True
 
+    def emptyline(self):
+        """Do nothing on empty line"""
+        pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
