92ec10371ecbf5f1574b9a956a0a1ce9389e0c81
Kymemia <kimemiachristopherr@gmail.com>
2024-03-09 15:55:45 +0300

    store first object

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,7 @@ class BaseModel:
     """Public instance attributes"""
     def __init__(self, *args, **kwargs):
         """Initialization of BaseModel class public instances"""
+        self.storage = kwargs.pop('_sa_instance_state', None)
         if kwargs:
             for key, value in kwargs.items():
                 if key != '__class__':
@@ -19,6 +20,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            if self.storage:
+                self.storage.new(self)
 
     def __str__(self):
         """string representation of the BaseClass"""
@@ -27,7 +30,12 @@ class BaseModel:
     def save(self):
         """ Public instance method that updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        if self.storage:
+            self.storage.save()
 
     def to_dict(self):
         """Public instance method that  returns a dictionary containing all keys/values of __dict__ of the instance"""
-        return self.__dict__
+        obj_dict = self.__dict__.copy()
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
