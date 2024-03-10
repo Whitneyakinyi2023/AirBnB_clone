f977bbd87dd8f6db70477232190e511c64d05bdb
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-03-10 01:34:15 -0800

    fixed base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """BaseModel that defines all common attributes/methods for other classes"""
 
-from datetime import datetime
 import uuid
-
-
-import uuid
-from . import storage
 from datetime import datetime
-from typing import Dict, Any
-
 
 class BaseModel:
-    """Public instance attributes"""
+    """Base class for other models"""
 
     def __init__(self, *args, **kwargs):
-        """Initialization of BaseModel class public instances"""
+        """Initialization method"""
+        from models import storage
         if kwargs:
             for key, value in kwargs.items():
-
-                if key == 'created_at' or key == 'updated_at':
+                if key in ['created_at', 'updated_at']:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != '__class__':
                     setattr(self, key, value)
@@ -28,32 +21,19 @@ class BaseModel:
             self.created_at = self.updated_at = datetime.now()
             storage.new(self)
 
-        if key != '__class__':
-            if key == 'created_at' or key == 'updated_at':
-                if isinstance(value, str):
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
     def __str__(self):
-        """string representation of the BaseClass"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """String representation of the object"""
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """ Public instance method that updates the publicbute updated_at """
-        self.updated_at = datetime.now()
-
-        storage.save()
+        """Saves the object"""
         from models import storage
-        if storage:
-            storage.new(self)
-            storage.save()
+        self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        """Public instance method that returns a dictiionary """
+        """Converts object to dictionary"""
         obj_dict = self.__dict__.copy()
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
--- models/user.py
+++ models/user.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""class user inheriting from Base class """
+"""class user inheriting from Base class"""
 
 
-from .base_model import BaseModel
-
-
-class User(BaseModel):
-    """Class user that imports fro m the BaseClass"""
-    email = str ""
-    password = str""
-    first_name = str""
-    last_name = str""
-=======
-"""class, User, that inherits from BaseModel"""
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
-    """class, User"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
->>>>>>> cb77b257c13c07434eb9cc4b8b447a2fac7902e1
+    """Class representing a user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
