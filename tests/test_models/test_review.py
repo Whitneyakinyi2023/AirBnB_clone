2a7fac058e027a37132dcba9b29e0a38fe1bd7e4
Whitneyakinyi2023 <whitneyakinyi732gmail.com>
2024-03-12 05:16:57 -0700

    new

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""testing for the review module"""
+from models.review import Review
+from models.base_model import BaseModel
+import unittest
+
+class test_Review(unittest.TestCase):
+    """test for the review class"""
+    def test_create(self):
+        """creation of instance of a review"""
+        review = Review()
+        self.assertIsInstance(review, Review)
+        self.assertIsInstance(review, BaseModel)
+        self.assertEqual(review.place_id, "")
+        self.assertIsInstance(review.place_id, str)
+        self.assertEqual(review.user_id, "")
+        self.assertIsInstance(review. user_id, str)
+        self.assertEqual(review.text, "")
+        self.assertIsInstance(review.text, str)
+
+if __name__ == "__main__":
+    unittest.main()
